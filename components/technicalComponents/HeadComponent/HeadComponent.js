81e5f856a211dae65a5bed6b54b3ba49ccadb334
BalderDemon <148006009+BalderDemon@users.noreply.github.com>
2023-12-18 23:23:14 +0100

    Update HeadComponent.js

--- components/technicalComponents/HeadComponent/HeadComponent.js
+++ components/technicalComponents/HeadComponent/HeadComponent.js
@@ -52,6 +52,7 @@ export default class HeadComponent extends Component {
 			<Head>
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Kalnia:wght@400;700&display=swap" rel="stylesheet">
 				<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
 				<meta lang="en" />
 				<link rel="icon" href="/images/logo/favicon@32x.png" />
